38f73f155a45b78392981ecca53d49df92da99f9
HABDUL LAURA <habdullaura@macbook-pro-de-habdul.home>
2022-08-10 00:13:38 +0200

    Initial commit

---
+++ src/main/java/com/genomic/Fibersmart/FibersmartApplication.java
@@ -0,0 +1,13 @@
+package com.genomic.Fibersmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibersmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FibersmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genomic/Fibersmart/FibersmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genomic.Fibersmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibersmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
