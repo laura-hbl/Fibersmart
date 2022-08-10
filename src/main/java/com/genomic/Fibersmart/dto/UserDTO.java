4ad404dbcf52f10b5c500b14467aae2a61b6a36c
laura-hbl <laura_habdul@hotmail.fr>
2022-08-10 15:05:11 +0200

    Adds UserDTO class

---
+++ src/main/java/com/genomic/Fibersmart/dto/UserDTO.java
@@ -0,0 +1,61 @@
+package com.genomic.Fibersmart.dto;
+
+public class UserDTO {
+
+    private Long id;
+
+    private String username;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String password;
+
+    public UserDTO(final String username, final String firstName, final String lastName, final String password) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(final String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+}
