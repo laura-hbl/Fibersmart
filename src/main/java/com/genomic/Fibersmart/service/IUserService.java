95b4b7187ff6daf428ecfa8f7974c94397a12f97
laura-hbl <laura_habdul@hotmail.fr>
2022-08-11 13:02:22 +0200

    Updates UserService

--- src/main/java/com/genomic/Fibersmart/service/IUserService.java
+++ src/main/java/com/genomic/Fibersmart/service/IUserService.java
@@ -1,4 +1,9 @@
 package com.genomic.Fibersmart.service;
 
+import com.genomic.Fibersmart.dto.UserDTO;
+
 public interface IUserService {
+
+    UserDTO addUser(UserDTO userDTO);
+
 }
--- src/main/java/com/genomic/Fibersmart/service/UserService.java
+++ src/main/java/com/genomic/Fibersmart/service/UserService.java
@@ -1,7 +1,39 @@
 package com.genomic.Fibersmart.service;
 
+import com.genomic.Fibersmart.dto.UserDTO;
+import com.genomic.Fibersmart.repository.UserRepository;
+import com.genomic.Fibersmart.util.DTOConverter;
+import com.genomic.Fibersmart.util.ModelConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements IUserService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
+
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    private final DTOConverter dtoConverter;
+
+    private final ModelConverter modelConverter;
+
+    @Autowired
+    public UserService(final UserRepository userRepository, final BCryptPasswordEncoder passwordEncoder,
+                       final DTOConverter dtoConverter, final ModelConverter modelConverter) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.dtoConverter = dtoConverter;
+        this.modelConverter = modelConverter;
+    }
+
+    @Override
+    public UserDTO addUser (UserDTO userDTO) {
+
+    }
 }
