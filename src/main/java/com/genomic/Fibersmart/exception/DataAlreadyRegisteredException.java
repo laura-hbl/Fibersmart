b516ae32eb34c67877346cd2a585317071c667f3
laura-hbl <laura_habdul@hotmail.fr>
2022-08-24 10:09:48 +0200

    Adds Swagger configuration and DataAlreadyRegisteredException

---
+++ src/main/java/com/genomic/Fibersmart/exception/DataAlreadyRegisteredException.java
@@ -0,0 +1,7 @@
+package com.genomic.Fibersmart.exception;
+
+public class DataAlreadyRegisteredException extends RuntimeException {
+    public DataAlreadyRegisteredException(final String message) {
+        super(message);
+    }
+}
