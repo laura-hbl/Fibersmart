fd85626e537c9fb9f650feae7d2a3e351012ee97
laura-hbl <laura_habdul@hotmail.fr>
2022-08-24 10:11:07 +0200

    Updates AuthorityConstants application.properties

--- src/main/java/com/genomic/Fibersmart/constants/AuthorityConstants.java
+++ src/main/java/com/genomic/Fibersmart/constants/AuthorityConstants.java
@@ -1,15 +1,37 @@
 package com.genomic.Fibersmart.constants;
 
+import com.genomic.Fibersmart.model.User;
+
 import java.io.Serializable;
 
+/**
+ * Static final permissions strings to be used in the controller classes for restricting access to api calls.
+ */
 public class AuthorityConstants {
 
-    public static final String PERM_ALL = "PERM_ALL";
-    public static final String LOGIN = "LOGIN";
-    public static final String CREATE_USER = "CREATE_USER";
-    public static final String VIEW_SLIDES = "VIEW_SLIDES";
+    //Creates, updates, deletes, gets all.
+    public static final String MANAGE_USER = "MANAGE_USER";
+
+    //Creates, updates, deletes, gets all.
+    public static final String MANAGE_USER_GROUP = "MANAGE_USER_GROUP";
+
+    //Creates, updates, deletes, gets all.
+    public static final String MANAGE_COVERSLIP = "MANAGE_COVERSLIP";
+
+    //Creates, updates, deletes, gets all.
+    public static final String MANAGE_SLIDE = "MANAGE_SLIDE";
 
 
+    //Group permissions.
+    public static final String SEE_ALL_SLIDES = "SEE_ALL_SLIDES";
+    public static final String DOWNLOAD_JSON = "DOWNLOAD_JSON";
+
+
+    /**
+     * The placeholder enum name for the {@link com.genomic.Fibersmart.model.UserRole} entity. It is used as a lookup
+     * id for retrieving the userRole, which itself contains a collection of {@link
+     * com.genomic.Fibersmart.model.RolePermission}.
+     */
     public enum RoleType implements Serializable {
         ROLE_ADMIN,
         ROLE_CLIENT,
