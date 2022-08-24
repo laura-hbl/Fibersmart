3bfd8780e7f403712a2ea11b5a4c6dcdd0ee5d86
laura-hbl <laura_habdul@hotmail.fr>
2022-08-24 10:08:16 +0200

    Updates User service classes

--- src/main/java/com/genomic/Fibersmart/service/IUserService.java
+++ src/main/java/com/genomic/Fibersmart/service/IUserService.java
@@ -1,9 +1,42 @@
 package com.genomic.Fibersmart.service;
 
 import com.genomic.Fibersmart.dto.UserDTO;
+import com.genomic.Fibersmart.model.User;
 
+import java.util.List;
+
+/**
+ * UserService interface.
+ */
 public interface IUserService {
 
-    UserDTO addUser(UserDTO userDTO);
+    /**
+     * Registers a new {@link User}.
+     *
+     * @param user the {@link User} to be registered
+     * @return The {@link User} saved
+     */
+    User createUser(final User user);
+
+    /**
+     * Deletes the {@link User} associated with the given id.
+     *
+     * @param userId id of the {@link User} to be deleted
+     */
+    void deleteUser(final Long userId);
+
+    /**
+     * Retrieves all users.
+     *
+     * @return The list of all users.
+     */
+    List<User> getAllUser();
+
+    /**
+     * Updates the {@link User} associated with the given id.
+     *
+     * @return The {@link User} updated.
+     */
+    User updateUser(final User user);
 
 }
--- src/main/java/com/genomic/Fibersmart/service/UserService.java
+++ src/main/java/com/genomic/Fibersmart/service/UserService.java
@@ -1,15 +1,20 @@
 package com.genomic.Fibersmart.service;
 
-import com.genomic.Fibersmart.dto.UserDTO;
+import com.genomic.Fibersmart.exception.DataAlreadyRegisteredException;
+import com.genomic.Fibersmart.model.User;
+import com.genomic.Fibersmart.model.UserRole;
 import com.genomic.Fibersmart.repository.UserRepository;
-import com.genomic.Fibersmart.util.DTOConverter;
-import com.genomic.Fibersmart.util.ModelConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
+/**
+ * Contains methods that allow interaction between {@link User} business logic and user repository.
+ */
 @Service
 public class UserService implements IUserService {
 
@@ -19,21 +24,68 @@ public class UserService implements IUserService {
 
     private final BCryptPasswordEncoder passwordEncoder;
 
-    private final DTOConverter dtoConverter;
-
-    private final ModelConverter modelConverter;
-
     @Autowired
-    public UserService(final UserRepository userRepository, final BCryptPasswordEncoder passwordEncoder,
-                       final DTOConverter dtoConverter, final ModelConverter modelConverter) {
+    public UserService(final UserRepository userRepository, final BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
-        this.dtoConverter = dtoConverter;
-        this.modelConverter = modelConverter;
+    }
+
+    /**
+     * Checks if username is associated with a registered {@link User}, if so DataAlreadyRegisteredException is thrown.
+     * Else, encrypts the password and saves the user.
+     *
+     * @param userData the {@link User} to be registered
+     * @return The {@link User} saved
+     */
+    @Override
+    public User createUser (final User userData) {
+
+        // the username must be unique for each user
+        User userFound = userRepository.findByUsername(userData.getUsername());
+
+        if (userFound != null) {
+            throw new DataAlreadyRegisteredException("The username provided may be registered already");
+        }
+
+        List<UserRole> userRoles = userData.getUserRoles();
+        userData.setUserRoles(userRoles);
+        userData.setPassword(passwordEncoder.encode(userData.getPassword()));
+        User userSaved = userRepository.save(userData);
+
+        return userSaved;
+    }
+
+    /**
+     * Checks if the given id is associated with a registered {@link User}, if so user is deleted.
+     * Else, ResourceNotFoundException is thrown.
+     *
+     * @param userId id of the {@link User} to be deleted
+     */
+    @Override
+    public void deleteUser(final Long userId) {
+
+        userRepository.findById(userId).orElseThrow(() ->
+                new ResourceNotFoundException("No user registered with this id"));
+
+        userRepository.deleteById(userId);
+    }
+
+    /**
+     * Retrieves all users.
+     */
+    @Override
+    public List<User> getAllUser() {
+
+        return userRepository.findAll();
     }
 
     @Override
-    public UserDTO addUser (UserDTO userDTO) {
+    public User updateUser(final User user) {
+
+        userRepository.findById(user.getId()).orElseThrow(() ->
+                new ResourceNotFoundException("No user registered with this id"));
+
+        return userRepository.save(user);
 
     }
 }
